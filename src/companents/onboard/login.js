43cc2a1bf698cf23092b8b8bbcbbf1bdb183b6d4
mahboubi-hub <rezamahbobi25@gmail.com>
2020-05-20 02:37:06 +0430

    validation

--- src/companents/onboard/login.js
+++ src/companents/onboard/login.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import logo from '../../logo.svg'
+import validate from '../../validation/validateFunction.js'
 
 
 class Login extends React.Component{
     constructor() {
         super()
-        this.state = {email:'',password:''}
+        this.state = {
+            email:'',
+            password:'',
+            emailError:null,
+            passwordError:null
+        
+        }
 
     }
+validationFealds(){
+    const emailError=validate('email',this.state.email)
+    const passwordlError=validate('password',this.state.password)
+    this.setState({emailError: emailError, passwordError:passwordlError})
+
+
+}
 
 handleChange (e) {
     console.log('ddddd',e.target.value )
 }
 handleChangeEmail (e) {
     let name = e.target.name
-    console.log('handleChangeEmail',e.target.value,this )
+    // console.log('handleChangeEmail',e.target.value,this )
     this.setState({[name]: e.target.value})
 }
 
@@ -36,17 +50,34 @@ handleChangeEmail (e) {
                      name='email'
                      onChange={(e) =>this.handleChangeEmail(e)} />
 
-                    <p>{this.state.email.value}</p>
-                    
+                     {
+                         this.state.emailError !==null &&
+
+                        <p style={{color:'red'}}>{this.state.emailError}</p>
+
+                     }
+
+
+
                     <input
                      placeholder='Password' 
                      type='password'
                      name='password'
                      onChange={(e) => this.handleChangeEmail(e)} />
                      
+                     
+                     {
+                         this.state.passwordError !==null &&
+
+                     <p style={{color:'red'}}>{this.state.passwordError}</p>
+
+                     }
+                     
 
                     
-                    <button ><b>login</b></button>
+                    <button 
+                        onClick={()=>this.validationFealds()}
+                    ><b>login</b></button>
                     </div>
 
             </div>
